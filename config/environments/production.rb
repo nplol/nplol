cd66602355a83213b51da64a226c7ad5f34de858
Nicolay Hvidsten <nicohvi@gmail.com>
2013-09-22 21:52:54 +0200

    added heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Nplol::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # Set serving to :true for Heroku.
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
