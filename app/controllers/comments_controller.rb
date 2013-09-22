ecc266c1cffa9680a0d307a9ef7e73b84176cfd9
Nicolay Hvidsten <nicohvi@gmail.com>
2013-09-22 21:51:18 +0200

    assets precompiled

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "2pac", password: "2pac"
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  http_basic_authenticate_with name: "2pac", password: "2pac", except: [:index, :show]
+
   def index
   	@posts = Post.all
   end
