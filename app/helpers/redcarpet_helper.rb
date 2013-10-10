f7e7cb959eb216652af6bd7b59c6d61ef064327d
Nicolay Hvidsten <nico.hvi@gmail.com>
2013-10-10 21:02:43 +0200

    added help for adding images

--- app/helpers/redcarpet_helper.rb
+++ app/helpers/redcarpet_helper.rb
@@ -15,7 +15,10 @@ class HTMLBlockCode < Redcarpet::Render::HTML
     puts "LINK"
     puts link.inspect
 
+    # bilder har format: ![alt text] (class src)
     klass = link.match(/([^\s]+)/)[0]
+    puts "KLASS"
+    puts klass.inspect  
     link = link.match(/[^ ]* (.*)/)[1]
 
     image_tag(link, size: size, title: title, alt: alt_text, class: klass)
