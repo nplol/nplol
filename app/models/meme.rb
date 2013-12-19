888185a1d3741efd77776868feffbe190e6ebd95
Nicolay Hvidsten <nico.hvi@gmail.com>
2013-12-19 15:40:19 +0100

    removed duplicate validation from meme model

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -2,9 +2,6 @@ class Meme < Post
   has_attached_file :image, styles: { large: '640x480', medium: '300x300>', thumb: '100x100>' }
 
   validates_attachment_content_type :image, :content_type => /^image\/(png|gif|jpeg|jpg)/
-  validates :title, 	presence: true,
-            length: {minimum: 5},
-            uniqueness: true
 
   # awesome hack: http://stackoverflow.com/questions/4507149/best-practices-to-handle-routes-for-sti-subclasses-in-rails
   def self.model_name
