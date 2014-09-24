fb02e833ad80841aa5d56cd9af8fce5aa573995f
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-09-24 11:33:48 +0200

    fixing user reference through USING

--- db/migrate/20140924092837_remove_uuid_as_id.rb
+++ db/migrate/20140924092837_remove_uuid_as_id.rb
@@ -1,7 +1,7 @@
 class RemoveUuidAsId < ActiveRecord::Migration
   def change
-    change_column :posts, :user_id, :integer
-    change_column :comments, :user_id, :integer
-    change_column :likes, :user_id, :integer
+    change_column :posts, :user_id, 'integer USING CAST(user_id AS integer)'
+    change_column :comments, :user_id, 'integer USING CAST(user_id AS integer)'
+    change_column :likes, :user_id, 'integer USING CAST(user_id AS integer)'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918132049) do
+ActiveRecord::Schema.define(version: 20140924092837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,14 +22,14 @@ ActiveRecord::Schema.define(version: 20140918132049) do
     t.datetime "updated_at"
     t.string   "name"
     t.text     "text"
-    t.string   "user_id"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "likes", force: true do |t|
-    t.string   "user_id"
+    t.integer  "user_id"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20140918132049) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "user_id"
+    t.integer  "user_id"
     t.boolean  "public",             default: true
   end
 
